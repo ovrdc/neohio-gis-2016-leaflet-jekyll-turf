067d9698273792c1269f3f5daa6c16e0cdb01476
ovrdc <gis.ovrdc@gmail.com>
2016-08-24 12:40:12 -0400

    first commit

---
+++ js/map-plugins.js
@@ -0,0 +1,11 @@
+---
+---
+/*Map plugins installed via npm*/
+/*omnivore*/
+{% include node_modules/leaflet-omnivore/leaflet-omnivore.min.js %}
+/*leaflet-providers**/
+{% include node_modules/leaflet-providers/leaflet-providers.js %}
+/*choropleth*/
+{% include node_modules/leaflet-choropleth/dist/choropleth.js %}
+/*turf*/
+{% include node_modules/turf/turf.min.js %}
